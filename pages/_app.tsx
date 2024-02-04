9b28d9da281d14c0357dddfdd9f13a6491d07314
Yones Maheri <yonesmaheri80@gmail.com>
2024-02-04 19:10:20 +0330

    add Roboto font to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
 
+import { Roboto } from "next/font/google";
+
+const inter = Roboto({ weight: ['100','400','700'], subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider className={inter.className}>
       <Component {...pageProps} />
     </NextUIProvider>
   );
