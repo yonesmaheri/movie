0a11b5767db1a8376c56e52aa33b8980e48444df
Yones Maheri <yonesmaheri80@gmail.com>
2024-02-06 17:17:54 +0330

    Update types

---
+++ app/types/movie.ts
@@ -0,0 +1,51 @@
+export type MoviesListType = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type SingleMovie = {
+  adult: boolean;
+  backdrop_path: string;
+  belong_to_collection: string | null | undefined;
+  budget: string;
+  genres: { id: number; name: string }[];
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string | null;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_language: { english_name: string; iso_39_1: string; name: string }[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
